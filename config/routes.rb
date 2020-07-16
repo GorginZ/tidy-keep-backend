4ea0f248d0bdf3408c273df5c6046d2f7bc5e33d
GorginZ <georgia.z.leng@gmail.com>
2020-07-16 18:41:34 +1000

    Service model, Services Controller, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 # This create action is setup by knock we get it through the inheritance from the auth_token_controller
+resources :services
  post "/login", to: "user_token#create"
  get "/status", to: "status#index"
 get "/status/user", to: "status#user"
