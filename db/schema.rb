79411eb614bc68745a6237c1c93c7d8b75c385c1
Susufung <suzzax@gmail.com>
2020-07-17 16:27:03 +1000

    migrated telephone number to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_17_053246) do
+ActiveRecord::Schema.define(version: 2020_07_17_061709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_07_17_053246) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
+    t.integer "phone"
   end
 
 end
