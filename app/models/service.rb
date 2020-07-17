a4151b3ac4e18a2f0b069ade294f1436b069cd96
Micah <Rus.Micah@gmail.com>
2020-07-17 16:48:37 +1000

    Set up relations between booking and service tables, through the use of the booking_service join table.

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,2 +1,6 @@
 class Service < ApplicationRecord
+
+  has_many :booking_services
+  has_many :bookings, through: :booking_services
+
 end
--- db/migrate/20200717061438_create_bookings.rb
+++ db/migrate/20200717061438_create_bookings.rb
@@ -6,7 +6,7 @@ class CreateBookings < ActiveRecord::Migration[6.0]
       t.boolean :recurring
 
       t.timestamps
-      # t.index ["user_id"], name: "index_bookings_on_user_id"
+      t.index ["user_id"], name: "index_bookings_on_user_id"
     end
   end
 end
