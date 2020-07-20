8ea874341ccb8cbed1262084a1c270fef660b5bf
Susufung <suzzax@gmail.com>
2020-07-20 16:35:15 +1000

    added associations to bookservice spec

--- spec/models/BookingService_spec.rb
+++ spec/models/BookingService_spec.rb
@@ -4,12 +4,16 @@ RSpec.describe BookingService, type: :model do
   subject {build(:BookingService)}
 
    context 'validations' do 
-    it 'is valid with valid attributes' do 
-    expect(subject).to be_valid 
+    it 'has a valid factory' do 
+    expect(build(:BookingService)).to be_valid 
    end 
-   it 'is not valid without a quantity' do
-     subject.quantity = nil
-     expect(subject).to_not be_valid 
-   end
+
+    it { expect(subject).to validate_presence_of (:quantity)}
+
   end 
+  context "associations" do 
+  it {expect(subject).to belong_to(:service)}
+  it {expect(subject).to belong_to(:booking)}
+  end
 end 
+
