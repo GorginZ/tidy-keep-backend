25a3029176cc6b904c6e0f433d4b70f152d1b683
Susufung <suzzax@gmail.com>
2020-07-20 16:42:25 +1000

    added associations to service spec

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -3,16 +3,19 @@ require 'rails_helper'
 RSpec.describe Service, type: :model do 
 subject {build(:service)}
    context 'validations' do 
-   it 'is valid with valid attributes' do 
-   expect(subject).to be_valid 
+   it 'has a valid factory' do 
+   expect(build(:booking)).to be_valid
    end 
-   it 'is not valid without a title' do
-     subject.title = nil
-     expect(subject).to_not be_valid 
-   end
-   it 'is not valid without a price' do
-     subject.price = nil
-     expect(subject).to_not be_valid 
-   end
+
+  it { expect(subject).to validate_presence_of (:title)}
+  it { expect(subject).to validate_presence_of (:price)}
   end 
+    context "associations" do
+    it {expect(subject).to have_many(:booking_services)}
+    it {expect(subject).to have_many(:bookings).through(:booking_services)}
+ 
+  end
 end 
+
+
+
