2b7685134b557d95b0474bf4ca7be70f7f37891e
Micah <Rus.Micah@gmail.com>
2020-07-24 13:54:29 +1000

    Added another address for a secondary user

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@ User.create(
 
   Address.create(user_id: 1, street_address: "65 fake Street", state: "NSW", post_code: 3137)
 
+  Address.create(user_id: 2, street_address: "120 Spencer Street", state: "VIC", post_code: 3003)
+
 
 Service.create(
   title: 'bathroom',
