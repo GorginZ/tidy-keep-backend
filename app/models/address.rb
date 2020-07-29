a2e924a1f411a395ab2c7570965625e6b2517f86
GorginZ <georgia.z.leng@gmail.com>
2020-07-29 14:19:35 +1000

    index for request is fine, added dependent-destroy association so that when address is deleted corresponding bookings are also deleted

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,6 @@
 class Address < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   validates :street_address, presence: true
   validates :state, presence: true
   validates :post_code, presence: true
